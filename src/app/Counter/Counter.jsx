69c745a7e10ea1af31a5a7567fe49a48a1352b27
RacheliShayzada <054racheli@gmail.com>
2024-11-04 14:47:59 +0200

    after fixing the css.

--- src/app/Counter/Counter.jsx
+++ src/app/Counter/Counter.jsx
@@ -10,7 +10,7 @@ const Counter = () => {
 
     return (
         <div>
-            <h1 className="title">Welcome to the Counter App</h1>
+            <h1 className="Countertitle">Welcome to the Counter App</h1>
             <h1 className="counter">Counter: {count}</h1>
         </div>
     )
--- src/app/Counter/page.jsx
+++ src/app/Counter/page.jsx
@@ -10,12 +10,12 @@ const CounterPage = () => {
     const { increment, decrement, reset } = useStore(); // משיכת המצב והפונקציות מהחנות
 
     return (
-        <div className="container">
+        <div className="Countercontainer">
             <Countrer />
             <div className="buttonContainer">
-                <button className="button" onClick={increment}>Increment</button>
-                <button className="button" onClick={decrement}>Decrement</button>
-                <button className="button" onClick={reset}>Reset</button>
+                <button className="Counterbutton" onClick={increment}>Increment</button>
+                <button className="Counterbutton" onClick={decrement}>Decrement</button>
+                <button className="Counterbutton" onClick={reset}>Reset</button>
             </div>
         </div>
     );
