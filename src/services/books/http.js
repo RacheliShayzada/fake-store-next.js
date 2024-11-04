b7efc9e7fed4e15f226b7a4c678c7fc27ed118ba
RacheliShayzada <054racheli@gmail.com>
2024-11-04 11:58:45 +0200

    after fixing the url again

--- src/services/books/http.js
+++ src/services/books/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: '/api', // גישה יחסית ל-API הפנימי
+    baseURL: '/', // גישה יחסית ל-API הפנימי
     headers: {
         'Content-Type': 'application/json',
         "access-control-allow-origin": "*"
